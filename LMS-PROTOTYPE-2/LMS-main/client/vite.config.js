04d2debcc3abc05c2365fedf0ef58f9d84220ab2
AniketSingh2309 <anisingh2309@gmail.com>
2026-05-03 12:26:57 +0530

    fix asset path for production

--- LMS-PROTOTYPE-2/LMS-main/client/vite.config.js
+++ LMS-PROTOTYPE-2/LMS-main/client/vite.config.js
@@ -2,9 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',   // FORCE RELATIVE PATHS
-  plugins: [react()],
-  build: {
-    assetsDir: 'assets'
-  }
-})
+  base: "/",   // ✅ THIS FIXES EVERYTHING
+});
