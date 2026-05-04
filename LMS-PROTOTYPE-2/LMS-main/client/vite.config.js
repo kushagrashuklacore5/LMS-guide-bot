d5c21f850d10520c2e20b5cb06a24b8ba24f0651
AniketSingh2309 <anisingh2309@gmail.com>
2026-05-04 15:02:00 +0530

    update build with relative paths configuration

--- LMS-PROTOTYPE-2/LMS-main/client/vite.config.js
+++ LMS-PROTOTYPE-2/LMS-main/client/vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/",   // 🔥 THIS IS THE FIX
+  base: './',   // FORCE RELATIVE PATHS
+  plugins: [react()],
+  build: {
+    assetsDir: 'assets'
+  }
 })
