800704d7778fefb0ad401a68fc2bfcdf7783832a
AniketSingh2309 <anisingh2309@gmail.com>
2026-05-04 11:16:27 +0530

    fix base path for production

--- LMS-PROTOTYPE-2/LMS-main/client/vite.config.js
+++ LMS-PROTOTYPE-2/LMS-main/client/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/",   // ✅ THIS FIXES EVERYTHING
-});
+  base: "/",   // 🔥 THIS IS THE FIX
+})
